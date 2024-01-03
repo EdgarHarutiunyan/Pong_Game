629a1ce8252c4d3c9850e27aa8764dbd13ebc8c4
Edgar Harutyunyan <edgharutyunyan@griddynamics.com>
2024-01-03 12:03:09 +0400

    Initial Commit of Main Method

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Main {
+    static JFrame frame = new JFrame("Pong Game");
+
+    public static void main(String[] args) {
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        frame.setResizable(false);
+
+        frame.setSize(650,550);
+
+        frame.setLocationRelativeTo(null);
+
+        PongGame game = new PongGame();
+
+        frame.add(game);
+
+        frame.setVisible(true);
+
+        Timer timer = new Timer(5, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                game.gameLogic();
+                game.repaint();
+            }
+        });
+
+        timer.start();
+    }
+}
